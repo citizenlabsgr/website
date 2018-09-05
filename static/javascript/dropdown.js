8b6985efa58b4ad5a01beb2762439ee1571ab9f0
Sam Bleckley <sam@climatum.com>
2018-09-04 20:34:21 -0400

    Make vaguely accessible

--- static/javascript/dropdown.js
+++ static/javascript/dropdown.js
@@ -2,7 +2,7 @@
 
 // var $newBtn = document.querySelector('#new')
 // var $main = document.querySelector('main')
-// 
+//
 // $newBtn.addEventListener('click', toggleMenu)
 // $main.addEventListener('click', toggleMenu)
 // function toggleMenu (event) {
@@ -17,17 +17,19 @@
 
 
 // Script to hide/show menu
-var span = document.querySelector('span#about_chevron');
-var menu = document.querySelector('ul.sub-menu');
-    span.addEventListener('click', function (event) {
-        if (menu.classList.contains('dn')) {
-            //menu.style.display = "none";
-            //span.innerHTML = "Show Menu";
-            menu.classList.remove('dn');
-        } else {
-            // menu.style.display = "";
-            // span.innerHTML = "Hide Menu";
-            menu.classList.add('dn');
+window.addEventListener("load", function() {
+    var span = document.querySelector('#about_chevron');
+    var menu = document.querySelector('ul.sub-menu');
+        span.addEventListener('click', function (event) {
+            if (menu.classList.contains('dn')) {
+                //menu.style.display = "none";
+                //span.innerHTML = "Show Menu";
+                menu.classList.remove('dn');
+            } else {
+                // menu.style.display = "";
+                // span.innerHTML = "Hide Menu";
+                menu.classList.add('dn');
+            }
         }
-    }
-);
+    );
+})
